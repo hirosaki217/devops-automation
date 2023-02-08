4e6d0274ddf55df89027a34bf51ec7bdc80cf764
hirosaki217 <hirosaki217@gmail.com>
2023-02-08 10:08:28 +0700

    test gradle

---
+++ src/main/java/my/app/hello/controller/MyController.java
@@ -0,0 +1,14 @@
+package my.app.hello.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MyController {
+    private String abc = "123";
+    @GetMapping("/")
+    public String index(){
+        return "- This is my web application!";
+    }
+}
